721327ba1c518315a5130ab0f0a8f376ead5a4a1
Your Name <agankin02@gmail.com>
2019-02-23 14:10:51 -0500

    through part 1 of lesson 1, but peer created instead of connected to master

---
+++ Assets/Scripts/PhotonNetworkManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonNetworkManager : Photon.MonoBehaviour {
+
+    [SerializeField] private Text connectText;
+    // Use this for initialization
+    void Start() {
+        PhotonNetwork.ConnectUsingSettings("v1");
+    }
+
+
+    public virtual void OnConnectedToMaster(){
+        Debug.Log("We are now connected to master");
+    }
+
+	// Update is called once per frame
+	void Update () {
+        connectText.text = PhotonNetwork.connectionStateDetailed.ToString();
+	}
+}
