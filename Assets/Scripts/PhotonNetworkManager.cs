4bff94bccd20aa3429093f2edd74f108229a4a47
Aaron Gankin <agankin02@gmail.com>
2019-02-26 21:35:29 -0500

    through second tutorial video

--- Assets/Scripts/PhotonNetworkManager.cs
+++ Assets/Scripts/PhotonNetworkManager.cs
@@ -6,14 +6,30 @@ using UnityEngine.UI;
 public class PhotonNetworkManager : Photon.MonoBehaviour {
 
     [SerializeField] private Text connectText;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject lobbyCamera;
+    [SerializeField] private Transform spawnPoint;
     // Use this for initialization
     void Start() {
         PhotonNetwork.ConnectUsingSettings("v1");
     }
 
 
-    public virtual void OnConnectedToMaster(){
-        Debug.Log("We are now connected to master");
+
+    public virtual void OnJoinedLobby(){
+        Debug.Log("We have now joined the lobby");
+
+       // RoomOptions roomOptions = new RoomOptions();
+        //join room if exists or create one
+        PhotonNetwork.JoinOrCreateRoom("New", null, null);
+    }
+
+    public virtual void OnJoinedRoom()
+    {
+        // Spawn in the player
+        PhotonNetwork.Instantiate(player.name, spawnPoint.position, spawnPoint.rotation, 0);
+        //deactivat elobby camera
+        lobbyCamera.SetActive(false);
     }
 
 	// Update is called once per frame
