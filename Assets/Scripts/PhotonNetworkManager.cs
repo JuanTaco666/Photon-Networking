d075f1248484657489d2bb977de4ad425e4e604f
Aaron Gankin <agankin02@gmail.com>
2019-03-01 06:32:19 -0500

    Through part 3 of video(two people in room)

--- Assets/Scripts/PhotonNetworkManager.cs
+++ Assets/Scripts/PhotonNetworkManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PhotonNetworkManager : Photon.MonoBehaviour {
+public class PhotonNetworkManager : MonoBehaviour {
 
     [SerializeField] private Text connectText;
     [SerializeField] private GameObject player;
